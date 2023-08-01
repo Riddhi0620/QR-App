b4674b9b64e397ad3a85a946149e721d92841ef0
Riddhi Tawde <91663956+Riddhi0620@users.noreply.github.com>
2023-08-02 02:14:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function qr() {
+    var t = document.getElementById("text").value;
+    document.getElementById("image").innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
+  }
